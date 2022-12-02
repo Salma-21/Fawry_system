c3297022515133a0dc926f62b1b1db00e31fbf39
mariem58 <100387600+mariem58@users.noreply.github.com>
2022-12-02 19:46:12 +0200

    Commit

---
+++ Fawry system/src/User.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class User {
+	    private int password;
+	    private String username;
+	    private String email;
+	    public boolean pay()
+	    {
+	      
+	    }
+
+}
