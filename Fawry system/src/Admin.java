9b28ea2054149bdc698597c51160d63900cfa8d1
Salma-21 <salma.ehamad@gmail.com>
2022-12-03 08:37:11 +0200

    add Service Provide r& search for service

--- Fawry system/src/Admin.java
+++ Fawry system/src/Admin.java
@@ -1,4 +1,13 @@
 
 public class Admin {
+	
+	public void add_Spicific_Discount(Discount d,String serviseName) {
+		
+		
+	}
+public void add_Overall_Discount(Discount d) {
+		
+		
+	}
 
 }
--- Fawry system/src/Boundry.java
+++ Fawry system/src/Boundry.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+import java.util.Vector;
 
 public class Boundry {
 	Sign sign;
+	//search
+	public Service searchForService() {
+		Service service;
+		System.out.println("Enter service name:");
+		Scanner obj1 = new Scanner(System.in);
+	    String str = obj1.nextLine();
+	    ServiceController ser=new ServiceController();
+	    service=ser.search(str);
+	    System.out.println(service); 
+	    return service;
+	}
 	
+	//add provider
+	public void addServiceProvider() {
+		Service s=new Service();
+		s=searchForService();
+		System.out.println("Enter provider name:");
+		Scanner obj1 = new Scanner(System.in);
+	    String provider_name = obj1.nextLine();
+	    System.out.println("Enter num of form fields:");
+	    int size = obj1.nextInt();
+	    Vector<String> vec=new Vector();
+	    for (int i=0; i<size ;i++)
+		{
+	    	System.out.println("Enter field"+i+1+":");
+	    	String string = obj1.nextLine();
+	    	vec.add(string);
+		}
+	    //Provider p=new Provider(provider_name,vec,);
+	    s.add_provider(p);
+		
+	}
 
 }
